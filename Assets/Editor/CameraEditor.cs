f21455d3bfcd8f0b8ea6538fe91d60cd0a819bf6
ABDURRAHMAN MERT SAFAK <e2171981@ceng.metu.edu.tr>
2020-02-17 13:16:57 +0300

    Add camera editor.

---
+++ Assets/Editor/CameraEditor.cs
@@ -0,0 +1,30 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using UnityEngine;
+using System.Collections;
+using Tools;
+#if UNITY_EDITOR
+namespace Editor
+{
+    [CustomEditor(typeof(CameraController))]
+    public class CameraEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+            CameraController cameraController = (CameraController) target;
+            cameraController.cullObjectFrontofTarget =
+                EditorGUILayout.Toggle("Cull Object between camera and Target",
+                    cameraController.cullObjectFrontofTarget);
+            if (cameraController.cullObjectFrontofTarget)
+            {
+                cameraController.transparency =
+                    EditorGUILayout.IntSlider("Transparency", cameraController.transparency,0,255);
+                cameraController.transparentMaterial = EditorGUILayout.ObjectField("Transparent Material",
+                    cameraController.transparentMaterial, typeof(Material)) as Material;
+            }
+        }
+    }
+}
+#endif
